ee51b9e52a6a32d4369322fd499f10a48f4f6e21
Anderson Neves <neves1697@gmail.com>
2025-01-27 11:53:36 -0300

    Teste gitgnore

--- Exercicios/obj/Debug/net9.0/Exercicios.AssemblyInfo.cs
+++ Exercicios/obj/Debug/net9.0/Exercicios.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Exercicios")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e7f0264315a346bfdd1a406c99be54adf508b707")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6036a67e7bade452a1848e37c68de834384d0eb1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Exercicios")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Exercicios")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- ScreenSound/obj/Debug/net9.0/ScreenSound.AssemblyInfo.cs
+++ ScreenSound/obj/Debug/net9.0/ScreenSound.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ScreenSound")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+251180f2617a86477d92c5ed63c70dfdc568d491")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6036a67e7bade452a1848e37c68de834384d0eb1")]
 [assembly: System.Reflection.AssemblyProductAttribute("ScreenSound")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ScreenSound")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
